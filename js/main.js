0b78060df213eb46ef8e306d4944d73147dad74c
Macarena <maquibarrios@gmail.com>
2024-01-25 14:33:06 -0300

    responsive tablet parcial

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 function toggleNavbar() {
   var navbar = document.getElementById('navbar');
-  if (navbar.style.display === 'none' || navbar.style.display === '') {
-    navbar.style.display = 'flex';
-  } else {
-    navbar.style.display = 'none';
-  }
+  // Verificar si el navbar está visible o no
+  var isNavbarVisible = window.getComputedStyle(navbar).display !== 'none';
+
+  // Cambiar el estado del navbar
+  navbar.style.display = isNavbarVisible ? 'none' : 'flex';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('toggleNavbarBtn').addEventListener('click', toggleNavbar);
-});
 
-// Para ocultar el navbar en responsive
-document.addEventListener("DOMContentLoaded", function () {
+  // Ocultar el navbar en responsive al hacer clic en logoContainer
   var logoContainer = document.getElementById("logoContainer");
   logoContainer.addEventListener("click", function () {
-    document.getElementById("navbar").style.display = "none";
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      document.getElementById("navbar").style.display = "none";
+    }
   });
 });
