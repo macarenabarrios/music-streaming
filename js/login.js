85cfc8ca31d25fecd31eba128bd1b4c99236bae7
Gabriel Urueña <gabrieluruena01@gmail.com>
2024-02-02 20:27:05 -0300

    se agrega login, sus estilos y su js

---
+++ js/login.js
@@ -0,0 +1,42 @@
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const Users = JSON.parse(localStorage.getItem('users')) || [];
+    const isAdmin = email === 'administrador@devtunes.com' && password === 'Devtunes2024';
+    const validUser = Users.find(user => user.email === email && user.password === password);
+
+    if (!validUser && !isAdmin) {
+        Swal.fire({
+            title: "Usuario y/o contraseña incorrectos!",
+            icon: "error"
+        });
+    } else {
+        const redirectUrl = isAdmin ? 'pagina_admin.html' : 'index.html';
+        Swal.fire({
+            title: `Bienvenido ${validUser.name || 'Administrador'}!`,
+            icon: "success"
+        }).then(() => {
+            localStorage.setItem('login_success', JSON.stringify(validUser));
+            window.location.href = redirectUrl;
+        });
+    }
+});
+
+//FUNCION MOSTRAR/OCULTAR CONTRASEÑA
+
+const contraseña = document.getElementById("password");
+const iconoContraseña = document.getElementById("iconoMostrarContraseña");
+
+iconoContraseña.addEventListener("click", () => {
+    if (contraseña.type === "password") { 
+        contraseña.type = "text";
+        iconoContraseña.style.opacity = 0.8;
+    } else {
+        contraseña.type = "password";
+        iconoContraseña.style.opacity = 0.2;
+    }
+});
